a1676f99c96bb98fe6520496f176b94280320984
Alexey Cherkasov <alcherkasov@gmail.com>
2018-09-15 17:03:52 +0500

    Add "body" block.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './app/blocks/body';
+
 import CatalogView from './app/views/catalog';
 
 ReactDOM.render(<CatalogView/>, document.getElementById('root'));
